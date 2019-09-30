e363f2cd60841894743c3b7a4ab6a876fe2abab8
irenapopova <irenejpopova6@gmail.com>
2019-09-30 21:26:55 +0200

    rename folder

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `../restorant/data/restaurants.json`;
+    return `../restaurant/data/restaurants.json`;
   }
 
   /**
